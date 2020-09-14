7b3bff6e28b176d9f8e6803e503420de34af0fe1
Ludwig Schubert <ludwigschubi1@gmail.com>
2020-09-14 18:49:28 +0200

    Add webpack config for scss

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+      mode: "development", devtool: "inline-source-map",
+      entry: [ "./src/index.ts" ], 
+      output: {
+          filename: 'index.js',
+          path: path.resolve(__dirname, 'dist'),
+      },
+      resolve: {
+          // Add `.ts` and `.tsx` as a resolvable extension.
+          extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
+      },
+      module: {
+          rules: [
+            { 
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                include: path.resolve(__dirname, './src')
+              },
+                { test: /\.tsx?$/, loader: "ts-loader" }, 
+          ]
+      }
+  }; 
