3ec903cd348d4f80955bff9478df7b67bb935a10
aarnav <aarnavbos@gmail.com>
2020-09-19 13:01:22 +0200

    Fix webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-      mode: "development", devtool: "inline-source-map",
-      entry: [ "./src/index.ts" ], 
-      output: {
-          filename: 'index.js',
-          path: path.resolve(__dirname, 'dist'),
-          library: 'vs-components',
-          libraryTarget: 'commonjs',
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "va-components",
+    libraryTarget: "umd",
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "./src"),
       },
-      resolve: {
-          // Add `.ts` and `.tsx` as a resolvable extension.
-          extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
-      },
-      module: {
-          rules: [
-            { 
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-                include: path.resolve(__dirname, './src')
-              },
-                { test: /\.tsx?$/, loader: "ts-loader" }, 
-          ]
-      }
-  }; 
+      { test: /\.(ts|tsx)$/, loader: "ts-loader" },
+    ],
+  },
+};
