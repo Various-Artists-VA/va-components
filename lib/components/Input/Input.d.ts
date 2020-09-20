7a7200516bea8e1dae5f9c9444472bb2fec54c3a
Ludwig Schubert <ludwigschubi1@gmail.com>
2020-09-20 20:19:21 +0200

    Add pre commit and post push hook

--- lib/components/Input/Input.d.ts
+++ lib/components/Input/Input.d.ts
@@ -1,22 +1,23 @@
 import React, { CSSProperties } from "react";
 export declare enum InputVariant {
-    large = "large",
-    medium = "default",
-    small = "small"
+  large = "large",
+  medium = "default",
+  small = "small",
 }
 export interface TextInputProps extends React.HTMLProps<HTMLInputElement> {
-    variant: InputVariant;
-    initialValue?: string;
-    placeholder?: string;
-    classNamePrefix?: string;
-    isClearable?: boolean;
-    label?: string;
-    name?: string;
-    icon?: string;
-    style?: CSSProperties;
-    onChange?: (event: React.FormEvent<HTMLInputElement>) => void;
-    onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
-    onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  variant: InputVariant;
+  initialValue?: string;
+  placeholder?: string;
+  classNamePrefix?: string;
+  className?: string;
+  isClearable?: boolean;
+  label?: string;
+  name?: string;
+  icon?: string;
+  style?: CSSProperties;
+  onChange?: (event: React.FormEvent<HTMLInputElement>) => void;
+  onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 export declare const TextInput: React.FC<TextInputProps>;
 export default TextInput;
